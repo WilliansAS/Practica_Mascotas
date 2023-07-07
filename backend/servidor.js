2f340e46a92cb74ccfbdf7bb35544b97632f9d74
Anthony <22393157@utcancun.edu.mx>
2023-07-07 12:52:20 -0500

    Resultado final

---
+++ backend/servidor.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+
+//CREAR LAS INSTANCIA DE EXPRESS
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+//CREAR LA CONEXION
+const conexion=mysql.createConnection({
+    server:'localhost',
+    user:'root',
+    password:'',
+    database:'mascotitas'
+});
+
+//VERIFICAMOS LA CONEXION
+conexion.connect(function(error){
+    if(error){
+        console.log("ERROR AL CONECTAR A LA BASE DE DATOS")
+    }else{
+        console.log("CONEXION EXITOSA");
+    }
+});
+
+// CONSULTAR LA LISTA DE MASCOTAS
+app.get('/obtenerMascotas',(peticion, respuesta)=>{
+     const sql="select * from mascota";
+     conexion.query(sql,(error,resultado)=>{
+     if(error) return respuesta.json({mensaje:"error"});
+     return respuesta.json({mensaje:"Exitoso",contenido:resultado});
+});
+});
+
+//Acceso a usuario
+app.post('/acceso',(peticion,respuesta)=>{
+     const sql="SELECT * FROM usuarios WHERE correo_electronico = ? AND contrasenia=?";
+     console.log(peticion.body);
+     conexion.query(sql,[peticion.body.correo_electronico,peticion.body.contrasenia],
+    (error,resultado)=>{
+     if(error) return respuesta.json({mensaje:"Error en la consulta"});
+     if(resultado.length > 0){
+         const token=jwt.sign({usuario:'administrador'},'12345',{expiresIn:'1d'});
+         respuesta.cookie(token);
+         return respuesta.json({Estatus:"CORRECTO",Usario:token})
+     }else{
+        return respuesta.json({Estatus:"ERROR",Error:"Usuario contraseña incorrecta"});
+     }
+     })
+    });
+     
+
+
+//INICIAR SERVIDOR
+app.listen(8082,() =>{
+    console.log("Servidor iniciado");
+})
